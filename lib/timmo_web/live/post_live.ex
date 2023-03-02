35b70bc424eda003ad220fcaff96e9db03f008a6
Timmo Verlaan <timmo@timmo.immo>
2023-03-02 22:13:15 +0100

    add anchors to headings in post

--- lib/timmo_web/live/post_live.ex
+++ lib/timmo_web/live/post_live.ex
@@ -12,4 +12,32 @@ defmodule TimmoWeb.PostLive do
      |> assign(:page_title, post.title)
      |> assign_meta(post)}
   end
+
+  @h2_regex ~r/<h2.*?>(.*?)<\/h2>/s
+  defp link_headings(content) do
+    Regex.replace(@h2_regex, content, fn match, title ->
+      id = header_to_id(title)
+
+      if id == "" do
+        match
+      else
+        """
+        <h2 id="#{id}">
+          <a href="##{id}" aria-hidden="true" tabindex="-1"><svg class="icon anchor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"/><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"/></svg></a>
+          #{title}
+        </h2>
+        """
+      end
+    end)
+  end
+
+  defp header_to_id(header) do
+    header
+    |> String.replace(~r/<.+>/, "")
+    |> String.replace(~r/&#\d+;/, "")
+    |> String.replace(~r/&[A-Za-z0-9]+;/, "")
+    |> String.replace(~r/\W+/u, "-")
+    |> String.trim("-")
+    |> String.downcase()
+  end
 end
